422ea09530564108a300985404f0fa5c5070acf9
Ezejanu <Ezejanu_nno@ymail.com>
2024-02-23 02:57:55 +0000

    task 0, corrections

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,11 +1,6 @@
 -- Check if the table exists before creating
-IF NOT EXISTS (SELECT 1 FROM information_schema.tables
-	WHERE table_name = 'users') THEN
-
-	-- Create the users table
-	CREATE TABLE users (
-		id SERIAL PRIMARY KEY,
-		email VARCHAR(255) NOT NULL UNIQUE,
-		name VARCHAR(255)
-	);
-END IF;
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
